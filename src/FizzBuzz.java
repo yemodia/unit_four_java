50d2acad0a00f02ddfcc306e8d77cf6a5a3d7381
diaye <yerim-oumar.dia@ssfs.org>
2019-12-05 12:09:47 -0500

    Need to correct FizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,3 +1,21 @@
 public class FizzBuzz {
-    public static String fizz
+    public static String fizzBuzz(int num) {
+        String finalString = "";
+        for (int first = 1; first <= num; first++) {
+            if (first % 3 == 0 && first % 5 == 0)
+                finalString += "FizzBuzz" + " ";
+            else if (first % 5 == 0)
+                finalString += "Buzz" + " ";
+            else if (first % 3 == 0)
+                finalString += "Fizz" + " ";
+            else
+                finalString += first + " ";
+
+        }
+        return finalString;
+    }
+
+    public static void main(String[] args){
+        System.out.println(fizzBuzz(100));
+    }
 }
