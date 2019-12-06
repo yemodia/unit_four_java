0e3638fddb8f525f68379e6dbaaa90d32a23e002
diaye <yerim-oumar.dia@ssfs.org>
2019-12-06 11:55:32 -0500

    Finished FizzBuzz program

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,15 +2,16 @@ public class FizzBuzz {
     public static String fizzBuzz(int num) {
         String finalString = "";
         for (int first = 1; first <= num; first++) {
-            if (first % 3 == 0 && first % 5 == 0)
-                finalString += "FizzBuzz" + " ";
-            else if (first % 5 == 0)
-                finalString += "Buzz" + " ";
-            else if (first % 3 == 0)
-                finalString += "Fizz" + " ";
-            else
-                finalString += first + " ";
-
+                if (first % 3 == 0 && first % 5 == 0)
+                    finalString += "FizzBuzz" + " ";
+                else if (first % 5 == 0)
+                    finalString += "Buzz" + " ";
+                else if (first % 3 == 0)
+                    finalString += "Fizz" + " ";
+                else
+                    finalString += first + " ";
+            if (first % 20 == 0)
+                finalString += "\n";
         }
         return finalString;
     }
